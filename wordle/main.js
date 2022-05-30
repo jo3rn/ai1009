bc78636e27f1d22159337985afd9ac67468a4614
jo3rn <12082310+jo3rn@users.noreply.github.com>
2022-05-30 16:59:01 +0200

    Update wordle week 6

--- wordle/main.js
+++ wordle/main.js
@@ -2,10 +2,27 @@ console.log("Bin da");
 
 const keyboardButtons = document.querySelectorAll(".keyboard-button");
 
+const enterPressed = () => {
+  console.log("enter gedrückt");
+};
+
+const trashPressed = () => {
+  console.log("trash gedrückt");
+};
+
 for (const keyboardButton of keyboardButtons) {
-  //   console.log(keyboardButton);
-  keyboardButton.addEventListener("click", (e) => {
-    const letter = keyboardButton.innerHTML;
-    console.log(letter);
-  });
+  if (keyboardButton.innerHTML === "enter") {
+    keyboardButton.addEventListener("click", enterPressed, false);
+    continue;
+  } else if (keyboardButton.id === "backspace") {
+    keyboardButton.addEventListener("click", trashPressed, false);
+    continue;
+  } else {
+    keyboardButton.addEventListener("click", (e) => {
+      const nextCell = document.querySelector(".row-cell:empty");
+      const letter = keyboardButton.innerHTML;
+      nextCell.innerHTML = letter;
+      console.log(letter);
+    });
+  }
 }
